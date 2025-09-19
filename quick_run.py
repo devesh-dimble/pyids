d4451ca105465b44af52852b2568394e10b29aee
devesh-dimble <dimble@smaecs.com>
2025-09-20 00:46:59 +0200

    Update quick_run.py

--- quick_run.py
+++ quick_run.py
@@ -4,8 +4,15 @@ ids_obj = readIDS("sample1.ids")       # needs ifctester installed (optional ext
 model = toPydantic(ids_obj)
 print(model.model_dump_json(indent=2))
 '''
+from pathlib import Path
 from pyids import readIDS, toPydantic
 
 ids_obj = readIDS("ids_files/IDS_demo_BIM-basis-ILS.ids") #sample1.ids
 model = toPydantic(ids_obj)
 print(model.model_dump_json(indent=2))
+
+Path("IDS_demo_BIM-basis-ILS.json").write_text(
+    model.model_dump_json(indent=2),
+    encoding="utf-8"
+)
+print("✅ JSON saved to IDS_demo_BIM-basis-ILS.json")
